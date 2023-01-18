c767b6d6b0fa7a6f8adb127699a9e6a9e81f4e08
Victoria050799 <balakaeva99qw@mail.ru>
2023-01-18 03:12:10 +0300

    оы

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
-const btnCreateList = document.getElementById("btn-create-list");
-const btnResetList = document.getElementById("btn-reset-list");
-const desk = document.getElementById("desk");
-const inputListName = document.getElementById("list-name");
+const btnCreateList = document.getElementById('btn-create-list');
+const btnResetList = document.getElementById('btn-reset-list');
+const desk = document.getElementById('desk');
+ const inputListName = document.getElementById('list-name');
+let counter = 1;
 
-let counter=1;
-
-let addList=(event)=>{
-    event.preventDeafault();
-    let list =document.createElement('li');
+let addList = (event) => {
+    event.preventDefault();
+    let list = document.createElement('li');
     list.classList.add('list');
 
     let h2 = document.createElement('h2');
     let editors = document.createElement('div');
-    
-   
-    let cardList= document.createElement('div');
-    cardList.classList.add('card-list');
 
-    let addCard =document.createElement('a');
+    let cardList = document.createElement('div');
+    cardList.classList.add('card-list');
+    Array.from(cardList);
+
+    let addCard = document.createElement('a');
     addCard.classList.add('add-card');
     addCard.innerText = 'Add Card +';
+    addCard.addEventListener('click', editList);
 
-    let changeIco =document.createElement('img');
-    changeIco.setAttribute('scr','https://cdn.icon-icons.com/icons2/67/PNG/512/mapediting_map_13573.png ');
+    let changeIco = document.createElement('img');
+    changeIco.setAttribute('src', 'https://cdn.icon-icons.com/icons2/67/PNG/96/mapediting_map_13573.png');
     changeIco.classList.add('edit-list');
-
-    let deleteIco =document.createElement('img');
-    deleteIco.setAttribute('scr','https://cdn.icon-icons.com/icons2/10/PNG/256/remove_delete_exit_close_1545.png');
+    changeIco.addEventListener('click', editList);
+    
+    let deleteIco = document.createElement('img');
+    deleteIco.setAttribute('src', 'https://cdn.icon-icons.com/icons2/102/PNG/96/exit_delete_17889.png');
     deleteIco.classList.add('delete-list');
+    deleteIco.addEventListener('click', editList);
+    
     let listName = document.getElementById('list-name').value;
-    if(listName ===""){
-        listName=`Без названия ${counter++}`;
-        
+
+    if(listName === '') {
+        listName = `No name ${counter++}`;
     }
 
-    h2.innerText=listName;
+    h2.innerText = listName;
     list.append(h2);
     list.append(editors);
     editors.append(changeIco);
@@ -43,42 +46,48 @@ let addList=(event)=>{
     list.append(cardList);
     desk.append(list);
 
-    btnResetList.addEventListener('click',(event)=>{
+    btnResetList.addEventListener('click', (event) => {
         list.remove(desk);
-        counter=1;
+        counter = 1;
     });
-    btnCreateList.addEventListener('click',addList);
 };
 
+btnCreateList.addEventListener('click', addList);
 
-const editList=(event)=>{
-    let obj =event.target;
-    if(obj.classList.contains('edit-list')){
-        let list=obj.closest('.list');
-        let h2= list.querySelector('h2');
-        h2.setAttribute('contenteditable','true');
+function editList(event) {
+    let obj = event.target;
+    if (obj.classList.contains('edit-list')) {
+        let list = obj.closest('.list');
+        let h2 = list.querySelector('h2');
+        h2.setAttribute('contenteditable', 'true');
         h2.focus();
-        if(obj.classList.contains('delete-list')){
-            let list=obj.closest('.list');
-            list.remove(obj);
-        }
+        h2.innerText = '';
+    }
+    if(obj.classList.contains('delete-list')) {
+        let list = obj.closest('.list');
+        list.remove(obj);
     }
 
-if (obj.classList.contains('add-card')){
-    let cardText=document.createElement('textarea');
-    cardText.classList.add('card-text');
-    let cardList= obj.closest('.list').querySelector('.card-list');
-    cardList.append(cardText);
-    let deleteIco =document.createElement('img');
-    deleteIco.setAttribute('scr','https://cdn.icon-icons.com/icons2/10/PNG/256/remove_delete_exit_close_1545.png');
-    deleteIco.classList.add('delete-text');
-    cardList.append(deleteIco);
+    if(obj.classList.contains('add-card')) {
+        let cardText = document.createElement('textarea');
+        cardText.classList.add('card-text');
+        let cardList = obj.closest('.list').querySelector('.card-list');
+        cardList.append(cardText);
+        
+        let deleteIco = document.createElement('img');
+        deleteIco.setAttribute('src', 'https://cdn.icon-icons.com/icons2/102/PNG/96/exit_delete_17888.png');
+        deleteIco.classList.add('delete-text');
+        deleteIco.addEventListener('click', (elem) => {
+            if(elem.target) cardList.removeChild(cardText);
+           elem.target.style.display = 'none';
+        })
+        cardList.append(deleteIco);
+    }
+    
+    if(obj.classList.contains('delete-text')) {
+        let text = obj.closest('.card-list').querySelector('.card-text');
+        text.remove(obj);
+        let delIco = obj.closest('.card-list').querySelector('.delete-text');
+        delIco.remove(obj);
+    }
 }
-if (obj.classList.contains('delete-text')){
-    let text = obj.closest('.card-list').querySelector('.card-text');
-    text.remove(obj);
-    let delIco =obj.closest('.card-list').querySelector('.delete-text');
-    delIco.remove(obj);
-}    
-}
-
